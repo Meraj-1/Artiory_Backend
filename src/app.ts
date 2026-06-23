bade52cfdf901ffa22c9863d8661ef31267adc94
Meraj-1 <belalraza158@gmail.com>
2026-06-23 07:41:26 +0530

    feat: add user authentication APIs

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API Running"
+  });
+});
+
+app.use("/api/auth", authRoutes);
+
+export default app;
